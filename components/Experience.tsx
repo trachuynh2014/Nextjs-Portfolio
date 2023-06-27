6dced2e0631f97c3459c213d699af3e5f3c4f0a8
Finn Huynh <trachuynh2014@gmail.com>
2023-06-26 23:24:59 -0400

    finish work experience layout

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,5 +1,32 @@
+"use client";
+import { textVariant } from "@/utils/motion";
+import { motion } from "framer-motion";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import withContainer from "./Container";
+import { experiences } from "@/constants";
+import ExperienceCard from "./ExperienceCard";
+
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className="section-sub-text">What I have done so far</p>
+        <h2 className="section-head-text">Work Experience.</h2>
+      </motion.div>
+
+      <div className="mt-20 flex flex-col">
+        <VerticalTimeline>
+          {experiences.map((exp, index) => (
+            <ExperienceCard key={index} experience={exp} />
+          ))}
+        </VerticalTimeline>
+      </div>
+    </>
+  );
 };
 
-export default Experience;
+export default withContainer(Experience, "experience");
---
+++ components/ExperienceCard.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import Image, { StaticImageData } from "next/image";
+import React from "react";
+
+import { VerticalTimelineElement } from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+
+interface ExperienceCardProps {
+  experience: {
+    title: string;
+    company_name: string;
+    icon: StaticImageData;
+    iconBg: string;
+    date: string;
+    points: string[];
+  };
+}
+
+const ExperienceCard: React.FC<ExperienceCardProps> = ({ experience }) => {
+  return (
+    <VerticalTimelineElement
+      contentStyle={{ background: "#1d1836", color: "#fff" }}
+      contentArrowStyle={{ borderRight: "7px solid #232631" }}
+      date={experience.date}
+      iconStyle={{ background: experience.iconBg }}
+      icon={
+        <div className="flex justify-center items-center w-full h-full">
+          <Image
+            src={experience.icon}
+            alt={experience.company_name}
+            className="object-contain"
+            fill
+          />
+        </div>
+      }
+    >
+      <div>
+        <h3 className="text-white text-[24px]">{experience.title}</h3>
+        <p className="text-secondary text-[16px] font-semibold m-0">
+          {experience.company_name}
+        </p>
+      </div>
+      <ul className="mt-5 list-disc ml-5 space-y-2">
+        {experience.points.map((point, index) => (
+          <li
+            key={`experience-point-${index}`}
+            className="text-white-100 text-[14px] pl-1 tracking-wider"
+          >
+            {point}
+          </li>
+        ))}
+      </ul>
+    </VerticalTimelineElement>
+  );
+};
+
+export default ExperienceCard;
