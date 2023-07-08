7da747b71dc41ed922bef2e8e1b7a0593e30aafb
Finn Huynh <trachuynh2014@gmail.com>
2023-07-07 21:57:17 -0400

    alter error message

--- app/error.tsx
+++ app/error.tsx
@@ -3,7 +3,9 @@
 const Error = () => {
   return (
     <div className="h-full flex items-center justify-center">
-      <div className="text-neutral-400">Something went wrong</div>
+      <div className="text-neutral-400">
+        Something went wrong. Please reload the page
+      </div>
     </div>
   );
 };
