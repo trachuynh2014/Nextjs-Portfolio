a7c66aa10f1d0f5b67b66e648bcb75f76fe08aa9
Finn Huynh <trachuynh2014@gmail.com>
2023-07-11 19:32:57 -0400

    fix responsive

--- app/components/ServiceCard.tsx
+++ app/components/ServiceCard.tsx
@@ -13,7 +13,7 @@ interface ServiceCardProps {
 
 const ServiceCard: React.FC<ServiceCardProps> = ({ title, icon, index }) => {
   return (
-    <Tilt className="xs:w-[250px] w-full">
+    <Tilt className="sm:w-[250px] w-full">
       <motion.div
         variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
         className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
